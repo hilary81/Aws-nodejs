90e67096619fd004db6d2c18da9ff0554467f279
hilary81 <hilary_yong@yahoo.com>
2022-09-01 12:57:12 +1200

    Build a simple api

---
+++ scripts/application_start.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+sudo chmod -R 777 /home/ec2-user/express-app
+
+# navigate into our wokring directory
+cd /home/ec2-user/express-app
+
+# add npm and node to path
+export NVM_DIR ="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+npm install
+
+node app.js > app.out.log 2> app.err.log < /dev/null &
